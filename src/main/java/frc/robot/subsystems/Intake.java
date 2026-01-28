8a53d670ff356cd63979b3f34c3102fc1f207958
John <70161896+jrleath@users.noreply.github.com>
2026-01-27 19:06:37 -0500

    Setup Intake
    I think

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,9 +6,14 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.commands.SparkPosition;
 import frc.robot.CanIDs;
 import frc.robot.Constants;
 
@@ -21,6 +26,41 @@ public class Intake extends SubsystemBase {
   {
     intakePivotMotor = new SparkFlex(CanIDs.INTAKE_PIVOT_MOTOR_ID, MotorType.kBrushless);
     intakeMotionMotor = new SparkFlex(CanIDs.INTAKE_MOTION_MOTOR_ID, MotorType.kBrushless);
+  
+    SparkFlexConfig intakePivotConfig = new SparkFlexConfig();
+      intakePivotConfig
+      .smartCurrentLimit(50)
+      .idleMode(IdleMode.kBrake);
+
+    SparkFlexConfig intakeMotionConfig = new SparkFlexConfig();
+      intakeMotionConfig
+      .smartCurrentLimit(50)
+      .idleMode(IdleMode.kBrake);
+  }
+
+  public Command intake()
+  {
+    return new InstantCommand(() -> intakeMotionMotor.set(0.5), this);
+  }
+
+  public Command intakeStop()
+  {
+    return new InstantCommand(() -> intakeMotionMotor.set(0));
+  }
+
+  public Command intakePivotPos(double position)
+  {
+    return new SparkPosition(intakePivotMotor, position, 1, this);
+  }
+
+  public Command intakeOut()
+  {
+    return intakePivotPos(0).withName("Intake Ready");
+  }
+
+  public Command intakeIn()
+  {
+    return intakePivotPos(0).withName("Intake Away");
   }
 
   @Override
